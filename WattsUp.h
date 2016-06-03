9427ce867e21fc12e335a839e84446d0d7272c14
Logan Goldberg <goldberg@grinnell.edu>
2016-06-02 21:03:58 -0500

    merge issue

--- WattsUp.h
+++ WattsUp.h
@@ -2,6 +2,10 @@
 #define __WATTSUP_H_
 
 #define MAX_STRING_LEN 256
+#define EXTERNAL_MODE 'E'
+#define INTERNAL_MODE 'I'
+#define TCPIP_MODE 'T'
+#define FULLHANDLING 2
 
 typedef enum { false, true } bool;
 
@@ -18,7 +22,7 @@ typedef struct flags {
     bool outfile;
     char *outfile_path;
   } outfile;
-  struct interval{
+  struct interval {
     bool active_interval;
     double interval;
   } interval;
@@ -28,6 +32,24 @@ typedef struct flags {
   } port;
 } flags;
 
+typedef struct WattsUp {
+  FILE* serial_file;
+  FILE* logfile;
+  int interval;
+  array_list *time;
+  array_list *power;
+  array_list *voltage;
+  array_list *current;  
+} WattsUp;
 
+WattsUp* initialize_wattsup(bool sim, char *port, int interval);
+
+void clean_wattsup(WattsUp *meter);
+
+void mode(WattsUp *meter, bool sim, char runmode);
+
+void fetch(WattsUp *meter, bool sim);
+
+void logging(WattsUp *meter, bool sim, char *logfile);
 
 #endif
