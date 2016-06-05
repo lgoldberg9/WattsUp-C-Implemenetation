51e262c46b3e9c82853d69fcc944a4b623b2a433
Logan Goldberg <goldberg@grinnell.edu>
2016-06-04 21:31:04 -0500

    added summary statistics and fixed seg fault

--- new-Main.c
+++ new-Main.c
@@ -15,74 +15,81 @@ const static char *dir = "/dev/ttyUSB0";
 int main(int argc, char **argv) {
 
   int fd = 0;
-  int ret = 0;
-  int intv = 10;
+  int intv = 20;
 
   fd = open(dir, O_RDWR);
 
   if (fd == -1) {
-    perror("failure: ");
+    perror("open failure");
+    return errno;
   }
 
   printf("fd: %d\n", fd);
   char buf[MAX_STRING_LEN];
   char line[MAX_STRING_LEN];
-  char* word;
+  char *word;
+  double amps = 0.0;
+  double volts = 0.0;
+  double watts = 0.0;
+  double tot_amps = 0.0;
+  double tot_volts = 0.0;
+  double tot_watts = 0.0;
+  int t = 0;
+  
   memset(&buf[0], 0, sizeof(buf));
 
-  // write(fd, LR, strlen(LR));
-  // printf("wrote LR\n");
-  
-  // for (int j = 0; j < intv; j++) {
-  //   read(fd, buf, MAX_STRING_LEN);
-  //   printf("j = %d: ", j);
-  //   for (int i = 0; i < MAX_STRING_LEN; i++) {
-  //     fprintf(stdout, "%c", buf[i]);
-  //   }
-  //   printf("\n");
-  // }
-
   write(fd, RW, strlen(RW));
-  printf("wrote RW\n");
+  printf("wrote RW: %s\n", RW);
+  usleep(2000000);
   
-  for (int j = 0; j < intv; j++) {
+  for (t = 1; t <= intv; t++) {
     
     read(fd, buf, MAX_STRING_LEN);
-    printf("j = %d: ", j);
     for (int i = 0; i < MAX_STRING_LEN; i++) {
-      //fprintf(stdout, "%c", buf[i]);
       line[i] = buf[i];
     }
 
     word = strtok(line, ",");
-    for (int m=0; m<=5 || word != NULL; m++){
-      switch(m){
-        case 3: printf("Amps = %s\t", word);
+
+    if (word != NULL) {
+      
+      printf("t = %d: ", t);
+      for (int m = 0; m <= 5; m++) {
+	switch(m) {
+	case 3:
+	  amps = atoi(word) / 1000.0;
+	  printf("Amps = %.4lf, ", amps);
+	  tot_amps += amps;
+	  break;
+	case 4:
+	  volts = atoi(word) / 10.0;
+	  printf("Volts = %.4lf, ", volts);
+	  tot_volts += volts;
+	  break;
+	case 5:
+	  watts = atoi(word) / 10.0;
+	  tot_watts += watts;
+	  printf("Watts = %.4lf, ", atoi(word) / 10.0);
+	  printf("Energy = %.4lf", tot_watts);
         break;
-        case 4: printf("Volts = %s\t", word);
-        break;
-        case 5: printf("Power = %s\t", word);
-        break;
-        default : break;
+	default:
+	  break;
+	}
+	word = strtok(NULL, ",");
       }
-      word = strtok(NULL, ",");
+      
+      putchar('\n');
+    } else {
+      t--;
     }
-    
-    printf("\n");
     usleep(1000000);
   }
 
-  // write(fd, LW, strlen(LW));
-  // printf("wrote LW\n");
-
-  // for (int j = 0; j < intv; j++) {
-  //   read(fd, buf, MAX_STRING_LEN);
-  //   printf("j = %d: ", j);
-  //   for (int i = 0; i < MAX_STRING_LEN; i++) {
-  //     fprintf(stdout, "%c", buf[i]);
-  //   }
-  //   printf("\n");
-  // }
+  fprintf(stdout, "======= Summary Statistics =======\n");
+  fprintf(stdout, "Average Amps:\t %.4lf A\n", tot_amps / t);
+  fprintf(stdout, "Average Volts:\t %.4lf V\n", tot_volts / t);
+  fprintf(stdout, "Average Watts:\t %.4lf W\n", tot_watts / t);
+  fprintf(stdout, "Total Energy:\t %.4lf J\n", tot_watts);
   
   return 0;
 
